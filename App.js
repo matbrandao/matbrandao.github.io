a81eec86187b13119b5627af8916cac72fe97b75
Mateus Brandão <mat_brandao@live.com>
2015-02-01 00:08:55 -0200

    cleaning up index

--- App.js
+++ App.js
@@ -2,24 +2,17 @@
 
   "use strict";
 
-  var App = angular.module("App", [
-    "App.controllers",
-    "App.services",
-    "App.directives",
-    "App.filters",
-    "ngRoute",
-    "ngResource"
-  ]);
+  var App = angular.module("App", []);
 
-  App.config(function ($routeProvider) {
-    $routeProvider
-      .when('/view1', {
-           templateUrl: 'view/view1.html'
-      })
-      .when('/view2', {
-           templateUrl: 'view/view2.html'
-      })
-      .otherwise({redirectTo : 'view1'});
-  });
+  // App.config(function ($routeProvider) {
+  //   $routeProvider
+  //     .when('/view1', {
+  //          templateUrl: 'view/view1.html'
+  //     })
+  //     .when('/view2', {
+  //          templateUrl: 'view/view2.html'
+  //     })
+  //     .otherwise({redirectTo : 'view1'});
+  // });
 
 }());
